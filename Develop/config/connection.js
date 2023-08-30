b0818c825494fa845cb67d4efa532ffea43864d9
ARS2989 <raysoloriojr@gmail.com>
2023-08-30 15:15:56 -0700

    changes to connection.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+module.exports = sequelize;
+
+module.exports = {
+    development: {
+      username: 'root',
+      password: 'Welcome3',
+      database: 'ecommerce_db',
       host: 'localhost',
-      dialect: 'mysql',
+      dialect: 'mysql'
       dialectOptions: {
         decimalNumbers: true,
-      },
-    });
-
-module.exports = sequelize;
+      }}}
